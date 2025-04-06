c99256c9f8cbb5013df50cd88eb9bcb4436b421d
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2025-04-06 09:31:31 -0300

    salida estandar de un programa

--- M1_introducicion/holaMundoCPP/Source.cpp
+++ M1_introducicion/holaMundoCPP/Source.cpp
@@ -5,5 +5,4 @@ int main() {
 	std::cout << "Hola mundo" << std::endl;
 
 	return 0;
-
 }
---
+++ M2_entradaSalidaEstandar/C01_salidaEstandar/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	
+	// mostrando texto en pantalla
+	std::cout << "Este es el texto que quiero que se vea" << std::endl;
+	std::cout << "Este es una segunda instrucion" << std::endl;
+
+	return 0;
+}
